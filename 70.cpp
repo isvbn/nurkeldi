cd7a638b61b1c0d225fbb35c5a488c25336d0893
nurkeldiiznat <57890202+nurkeldiiznat@users.noreply.github.com>
2019-11-20 21:16:04 +0600

    Create 70.cpp

---
+++ 70.cpp
@@ -0,0 +1,11 @@
+#include<iostream>
+#include<cmath>
+#include<string>
+using namespace std;
+int main(){
+    int x1,y1,x2,y2,x3,y3,x4,y4;
+    cin>>x1>>y1>>x2>>y2>>x3>>y3;
+    x4=x1;
+    y4=y3;
+    cout<<x4<<" "<<y4;
+}
